4426d40057e52da04fa7e90cbcbb0b618bbab03d
Obtains <97128583+Obtains@users.noreply.github.com>
2022-08-19 21:28:15 -0400

    Updates

---
+++ precache-manifest.19bb5116443d67a89f658838fc88ec89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e30c0c6cb909e24662426833eabaac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bea54bedae67a04a45c6",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "3e25ebd0684000ebf573",
+    "url": "/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "bea54bedae67a04a45c6",
+    "url": "/static/js/2.f93f22f1.chunk.js"
+  },
+  {
+    "revision": "0297ac78c823ef9b172e38637e06e92e",
+    "url": "/static/js/2.f93f22f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e25ebd0684000ebf573",
+    "url": "/static/js/main.1e69c7a2.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
